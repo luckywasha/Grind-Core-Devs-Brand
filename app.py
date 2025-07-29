1bcfd3ff25490d5e0804136b52e64a8325baa39e
zack <nyagwayaezekiah@gmail.com>
2025-07-30 01:13:20 +0300

    ...

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello Wash")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Render the home page."""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
